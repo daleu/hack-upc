6f4d3f2d68439e8dd098d8a42545bf949ae4d696
jmotger <joaquim.motger@est.fib.upc.edu>
2016-02-20 02:57:05 +0100

    testapp added

---
+++ flaskr/schema.sql
@@ -0,0 +1,12 @@
+drop table if exists entries;
+drop table if exists users;
+create table entries (
+	id integer primary key autoincrement,
+	title text not null,
+	text text not null
+);
+create table users (
+	id integer primary key autoincrement,
+	username text not null,
+	pass text not null
+);
